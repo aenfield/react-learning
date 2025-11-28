e7c397d076dd5c7239d5e2c07334b3b177028a29
Andrew Enfield <aenfield@gmail.com>
2025-11-28 15:28:40 -0800

    Updated eslint config for .test.js files.

--- test-driven-fizzbuzz/.eslintrc.js
+++ test-driven-fizzbuzz/.eslintrc.js
@@ -1,9 +1,15 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-    plugins: ['jest'],
-    extends: ['eslint:recommended', 'plugin:jest/recommended'],
+    extends: ['eslint:recommended'],
     env: {
         node: true,
         es2021: true,
-    }
+    },
+    overrides: [
+        {
+            files: ['*.test.js'],
+            plugins: ['jest'],
+            extends: ['plugin:jest/recommended']
+        },
+    ],
 };
