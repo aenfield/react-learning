89174feac2ccbc3025424d9bfe07e4fa5e0afba3
Andrew Enfield <aenfield@gmail.com>
2025-11-28 15:58:59 -0800

    Added some tests per the ch2 text.

--- test-driven-fizzbuzz/palindromes.test.js
+++ test-driven-fizzbuzz/palindromes.test.js
@@ -6,4 +6,18 @@ describe(`palindromes()`, () => {
     expect(palindromes("racecar")).toEqual([`racecar`]);
     expect(palindromes("fooboof")).toEqual(["fooboof"]);
   });
+  it("returns an empty array when given no palindromes", () => {
+    expect(palindromes("tic tac toe")).toEqual([]);
+  });
+  it("ignores casing", () => {
+    expect(palindromes("WoW")).toEqual(["wow"]);
+  });
+  it("ignores punctuation", () => {
+    expect(palindromes("yo, banana boy!")).toEqual("yobananaboy");
+  });
+  it("detected multi-word palindromes", () => {
+    expect(palindromes("A man, a plan, a canal, Panama")).toEqual([
+      "amanaplanacanalpanama",
+    ]);
+  });
 });
