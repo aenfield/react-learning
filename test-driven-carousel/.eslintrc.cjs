bd244ae216369dfd61efa3e3629faaaeb65ff8c2
Andrew Enfield <aenfield@gmail.com>
2025-11-29 11:28:26 -0800

    Configured eslint and prettier.

---
+++ test-driven-carousel/.eslintrc.cjs
@@ -0,0 +1,9 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  extends: ["eslint:recommended", "prettier"],
+  env: {
+    node: true,
+    es2021: true,
+  },
+  overrides: [{ files: ["*.cjs"] }],
+};
