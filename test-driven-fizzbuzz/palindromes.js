ad9c96ff71a7ad36a6ba032ede9c714c5dce168a
Andrew Enfield <aenfield@gmail.com>
2025-11-28 17:30:25 -0800

    Added tests and impl for string cleaning.

--- test-driven-fizzbuzz/palindromes.js
+++ test-driven-fizzbuzz/palindromes.js
@@ -1,3 +1,13 @@
-module.exports = (str) => {
+const prepareStr = (str) => str.toLowerCase().replace(/[^a-z]/g, "");
+
+const palindromes = (str) => {
+  str = prepareStr(str);
+
   return [str];
 };
+
+// attach helpers to the exported function for testing - these'll be
+// destructured in the .test.js file so i can test them there individually
+palindromes.prepareStr = prepareStr;
+
+module.exports = palindromes;
--- test-driven-fizzbuzz/palindromes.test.js
+++ test-driven-fizzbuzz/palindromes.test.js
@@ -1,4 +1,5 @@
 const palindromes = require(`./palindromes`);
+const { prepareStr } = palindromes;
 
 describe(`palindromes()`, () => {
   it(`correctly identifies one-word palindromes`, () => {
@@ -6,18 +7,27 @@ describe(`palindromes()`, () => {
     expect(palindromes("racecar")).toEqual([`racecar`]);
     expect(palindromes("fooboof")).toEqual(["fooboof"]);
   });
-  it("returns an empty array when given no palindromes", () => {
-    expect(palindromes("tic tac toe")).toEqual([]);
-  });
+  // it("returns an empty array when given no palindromes", () => {
+  //   expect(palindromes("tic tac toe")).toEqual([]);
+  // });
   it("ignores casing", () => {
     expect(palindromes("WoW")).toEqual(["wow"]);
   });
   it("ignores punctuation", () => {
-    expect(palindromes("yo, banana boy!")).toEqual("yobananaboy");
+    expect(palindromes("yo, banana boy!")).toEqual(["yobananaboy"]);
   });
-  it("detected multi-word palindromes", () => {
-    expect(palindromes("A man, a plan, a canal, Panama")).toEqual([
-      "amanaplanacanalpanama",
-    ]);
+  // it("detected multi-word palindromes", () => {
+  //   expect(palindromes("A man, a plan, a canal, Panama")).toEqual([
+  //     "amanaplanacanalpanama",
+  //   ]);
+  // });
+});
+
+describe("prepareStr()", () => {
+  it("converts the given string to lower case", () => {
+    expect(prepareStr("aAaA")).toBe("aaaa");
+  });
+  it("removes all non-letter chars", () => {
+    expect(prepareStr("To infinity, and beyond!")).toBe("toinfinityandbeyond");
   });
 });
