0fc4abee114bf6f61b3e7f86581431bd01b7776b
Andrew Enfield <aenfield@gmail.com>
2025-11-29 11:40:03 -0800

    Added eslint stuff for typescript and react.

--- test-driven-carousel/.eslintrc.cjs
+++ test-driven-carousel/.eslintrc.cjs
@@ -1,9 +1,34 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: ["eslint:recommended", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "prettier",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+  ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
   env: {
     node: true,
     es2021: true,
   },
-  overrides: [{ files: ["*.cjs"] }],
+  overrides: [
+    { files: ["*.cjs"] },
+    {
+      files: [".ts", "*.tsx"],
+      extends: [
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+      ],
+      plugins: ["@typescript-eslint"],
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: __dirname,
+      },
+    },
+  ],
 };
