ffc0b8ab520621aa1439e9e4fda9a7313361919c
Andrew Enfield <aenfield@gmail.com>
2025-11-28 18:02:33 -0800

    Added the rest of the tests and  passing impl.

--- test-driven-fizzbuzz/palindromes.js
+++ test-driven-fizzbuzz/palindromes.js
@@ -1,13 +1,56 @@
-const prepareStr = (str) => str.toLowerCase().replace(/[^a-z]/g, "");
+/* Not my impl - this is (more or less) what's in the book in ch2 */
 
 const palindromes = (str) => {
-  str = prepareStr(str);
+  str = prepareStr(str); // lower case and remove extra chars and whitespace
 
-  return [str];
+  const matches = [];
+  let startIndex = 0;
+  while (startIndex < str.length / 2) {
+    const palindrome = stringStartPalindrome(str.substring(startIndex));
+    if (palindrome) {
+      matches.push(palindrome);
+      startIndex += palindrome.length;
+    } else {
+      startIndex++;
+    }
+  }
+
+  return matches;
+};
+
+const prepareStr = (str) => str.toLowerCase().replace(/[^a-z]/g, "");
+
+const isPalindrome = (str) => {
+  // iterate over the first half of the string, checking to see if the matching
+  // char at the other end of the str is the same - if this ever isn't the case,
+  // it's not a palindrome; for strengths w/ odd lengths since JS doesn't do int
+  // division, I think it'll compare the char to itself
+  const endIndex = str.length - 1;
+  for (let i = 0; i < str.length / 2; i++) {
+    if (str[i] !== str[endIndex - i]) return false;
+  }
+  return true;
+};
+
+const stringStartPalindrome = (str) => {
+  // returns the longest palindrome at the start of the str, or null if no
+  // palindrome of length 3+ starts the string (I think)
+  const firstLetter = str[0];
+  let lastIndex = str.lastIndexOf(firstLetter);
+  while (lastIndex >= 2) {
+    const candidate = str.substring(0, lastIndex + 1);
+    if (isPalindrome(candidate)) {
+      return candidate;
+    }
+    lastIndex = str.lastIndexOf(firstLetter, lastIndex - 1);
+  }
+  return null;
 };
 
 // attach helpers to the exported function for testing - these'll be
 // destructured in the .test.js file so i can test them there individually
 palindromes.prepareStr = prepareStr;
+palindromes.isPalindrome = isPalindrome;
+palindromes.stringStartPalindrome = stringStartPalindrome;
 
 module.exports = palindromes;
--- test-driven-fizzbuzz/palindromes.test.js
+++ test-driven-fizzbuzz/palindromes.test.js
@@ -1,5 +1,5 @@
 const palindromes = require(`./palindromes`);
-const { prepareStr } = palindromes;
+const { prepareStr, isPalindrome, stringStartPalindrome } = palindromes;
 
 describe(`palindromes()`, () => {
   it(`correctly identifies one-word palindromes`, () => {
@@ -7,20 +7,44 @@ describe(`palindromes()`, () => {
     expect(palindromes("racecar")).toEqual([`racecar`]);
     expect(palindromes("fooboof")).toEqual(["fooboof"]);
   });
-  // it("returns an empty array when given no palindromes", () => {
-  //   expect(palindromes("tic tac toe")).toEqual([]);
-  // });
+  it("returns an empty array when given no palindromes", () => {
+    expect(palindromes("tic tac toe")).toEqual([]);
+  });
   it("ignores casing", () => {
     expect(palindromes("WoW")).toEqual(["wow"]);
   });
   it("ignores punctuation", () => {
     expect(palindromes("yo, banana boy!")).toEqual(["yobananaboy"]);
   });
-  // it("detected multi-word palindromes", () => {
-  //   expect(palindromes("A man, a plan, a canal, Panama")).toEqual([
-  //     "amanaplanacanalpanama",
-  //   ]);
-  // });
+  it("detected multi-word palindromes", () => {
+    expect(palindromes("A man, a plan, a canal, Panama")).toEqual([
+      "amanaplanacanalpanama",
+    ]);
+  });
+});
+
+describe("isPalindrome()", () => {
+  it("returns true when given a palindrome", () => {
+    expect(isPalindrome("aba")).toBe(true);
+    expect(isPalindrome("abba")).toBe(true);
+  });
+  it("returns false when given a non-palindrome", () => {
+    expect(isPalindrome("abb")).toBe(false);
+    expect(isPalindrome("aaba")).toBe(false);
+  });
+});
+
+describe("stringStartPalindrome()", () => {
+  it("returns the longest palindrome at the start of the passed string", () => {
+    expect(stringStartPalindrome("wow")).toBe("wow");
+    expect(stringStartPalindrome("ahha")).toBe("ahha");
+    expect(stringStartPalindrome("hohoho")).toBe("hohoh");
+  });
+  it("returns null if no length 3+ palindrome starts the passed string", () => {
+    expect(stringStartPalindrome("ww")).toBe(null);
+    expect(stringStartPalindrome("abcda")).toBe(null);
+    expect(stringStartPalindrome("bananarama")).toBe(null);
+  });
 });
 
 describe("prepareStr()", () => {
